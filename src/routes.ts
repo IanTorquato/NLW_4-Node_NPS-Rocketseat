b0ffd811828c2a7f72b4b3c63e13e39c2fff456e
IanTorquato <iantorquato2@gmail.com>
2021-02-24 16:35:12 -0300

    All surveys related files

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 
 import { UserController } from './controllers/UserController'
+import { SurveysController } from './controllers/SurveysController'
 
 const routes = Router()
 
 const userController = new UserController()
+const surveyController = new SurveysController()
 
 routes.post('/users', userController.create)
+
+routes.post('/surveys', surveyController.create)
 
 export { routes }
