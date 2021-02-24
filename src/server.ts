0ed9490920b8993af5a7469210dcbeb973bd781e
IanTorquato <iantorquato2@gmail.com>
2021-02-24 17:47:26 -0300

    Test of creating of users

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
-import 'reflect-metadata'
-import express from 'express'
+import 'dotenv/config'
 
-import './database'
-import { routes } from './routes'
-
-const app = express()
-
-app.use(express.json())
-app.use(routes)
+import { app } from './app'
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
