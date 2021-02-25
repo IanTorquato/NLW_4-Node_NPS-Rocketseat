35c1ce1ab2ed2053fc7e695425d8632b13094792
IanTorquato <iantorquato2@gmail.com>
2021-02-25 13:55:11 -0300

    Details

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -29,7 +29,8 @@ class SendMailController {
 		}
 
 		const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-			where: [{ user_id: user.id }, { value: null }]
+			where: [{ user_id: user.id }, { value: null }],
+			relations: ['user', 'survey']
 		})
 
 		const link = process.env.URL_MAIL
